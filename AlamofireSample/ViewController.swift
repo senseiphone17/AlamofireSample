ec8c38ac0586f9bcbf768d3b32f704e017bb45d1
senseiswift <senseiswift@gmail.com>
2017-01-01 17:03:22 +0900

    Alamofire

--- AlamofireSample/ViewController.swift
+++ AlamofireSample/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    let table = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        table.frame = view.frame
+        view.addSubview(table)
+
+        getArticles()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    func getArticles() {
+        Alamofire.request("https://qiita.com/api/v2/items")
+            .responseJSON { response in
+                print(response.result.value as Any)
+        }
+    }
 }
 
