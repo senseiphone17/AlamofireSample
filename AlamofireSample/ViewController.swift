31c907fe44832ff3e27f18dfa327244a1ee0ba66
senseiswift <senseiswift@gmail.com>
2017-01-01 16:00:09 +0900

    Initial Commit

---
+++ AlamofireSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlamofireSample
+//
+//  Created by sensei on 2017/01/01.
+//  Copyright © 2017年 sensei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
